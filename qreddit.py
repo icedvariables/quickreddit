f7b5226c09df6a268a12c959114f163e4d984830
icedvariables <macodeblacko@gmail.com>
2015-06-06 12:46:21 +0100

    Changed 'helpt' to 'help'

--- qreddit.py
+++ qreddit.py
@@ -6,7 +6,7 @@ def main():
 def parseArguments():
     parser = optparse.OptionParser()
     parser.add_option("-s", "--subreddit", help="Specify subreddit", dest="subreddit")
-    parser.add_option("-t", "--title", helpt="Specify title", dest="title")
+    parser.add_option("-t", "--title", help="Specify title", dest="title")
 
     return parser.parse_args()
 
