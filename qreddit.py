7bb774f1f27fc30687fd3ec8eec7a9f6cb4458ea
icedvariables <macodeblacko@gmail.com>
2015-06-06 12:54:30 +0100

    Added --body -b option

--- qreddit.py
+++ qreddit.py
@@ -7,6 +7,7 @@ def parseArguments():
     parser = optparse.OptionParser()
     parser.add_option("-s", "--subreddit", help="Specify subreddit", dest="subreddit")
     parser.add_option("-t", "--title", help="Specify title", dest="title")
+    parser.add_option("-b", "--body", help="Specify post body", dest="body")
 
     return parser.parse_args()
 
