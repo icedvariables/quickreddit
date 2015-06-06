3f3b061ccb803d44a3bf2ebe773f47454f04a99b
icedvariables <macodeblacko@gmail.com>
2015-06-06 13:45:45 +0100

    Added Reddit helper class

---
+++ reddit.py
@@ -0,0 +1,9 @@
+import praw
+
+class Reddit:
+    def __init__(self, username, password, userAgent="QuickReddit: http://github.com/icedvariables/quickreddit /u/icedvariables"):
+        self.r = praw.Reddit(user_agent=userAgent)
+        self.r.login(username, password)
+
+    def doTextPost(options):
+        self.r.submit(options["subreddit"], options["title"], text=options["body"])
