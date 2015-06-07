c1bf2c88e536eb3568e80c27165fbd3de519fa20
icedvariables <macodeblacko@gmail.com>
2015-06-07 13:29:31 +0100

    Made main file classy

--- qreddit.py
+++ qreddit.py
@@ -1,57 +1,57 @@
 import optparse, os, json
 from reddit import Reddit
 
-r = Reddit()
 
-def main():
-    users = getUsers()
-    print users
-    (options, args) = parseArguments()
+class QReddit:
+    def __init__(self):
+        self.r = Reddit()
+        self.users = self.getUsers()
+        (self.options, args) = self.parseArguments()
 
-    if(len(args) < 1):
-        print "Please specify type of action (textpost, linkpost)"
-        return
+        if(len(args) < 1):
+            print "Please specify type of action (textpost, linkpost)"
+            return
 
-    action = args[0]
+        self.action = args[0]
 
-    performAction(action, options)
+    def parseArguments(self):
+        parser = optparse.OptionParser()
+        parser.add_option("-s", "--subreddit", help="Specify subreddit", dest="subreddit")
+        parser.add_option("-t", "--title", help="Specify title", dest="title")
+        parser.add_option("-b", "--body", help="Specify post body (for text post)", dest="body")
+        parser.add_option("-l", "--link", help="Specify post link (for link post)", dest="link")
 
-def parseArguments():
-    parser = optparse.OptionParser()
-    parser.add_option("-s", "--subreddit", help="Specify subreddit", dest="subreddit")
-    parser.add_option("-t", "--title", help="Specify title", dest="title")
-    parser.add_option("-b", "--body", help="Specify post body (for text post)", dest="body")
-    parser.add_option("-l", "--link", help="Specify post link (for link post)", dest="link")
+        return parser.parse_args()
 
-    return parser.parse_args()
+    def performAction(self):
+        if(self.action == "textpost"):
+            self.r.doTextPost(self.options)
+        if(self.action == "linkpost"):
+            self.r.doLinkPost(self.options)
 
-def performAction(action, options):
-    if(action == "textpost"):
-        r.doTextPost(options)
-    if(action == "linkpost"):
-        r.doLinkPost(options)
+    def getUsers(self):
+        files = self.getUserFiles()
+        userData = []
 
-def getUsers():
-    files = getUserFiles()
-    userData = []
+        for filename in files:
+            with open(filename) as f:
+                try:
+                    userData.append(json.load(f))
+                except ValueError, e:
+                    print "Invalid user file:", e
+                    sys.exit()
 
-    for filename in files:
-        with open(filename) as f:
-            try:
-                userData.append(json.load(f))
-            except ValueError, e:
-                print "Invalid user file:", e
-                sys.exit()
+        return userData
 
-    return userData
+    def getUserFiles(self, directory="users"):
+        userFiles = []
 
-def getUserFiles(directory="users"):
-    userFiles = []
+        for file in os.listdir(directory):
+            if(file.split(".")[-1] == "user"):
+                userFiles.append(os.path.join(directory, file))
 
-    for file in os.listdir(directory):
-        if(file.split(".")[-1] == "user"):
-            userFiles.append(os.path.join(directory, file))
+        return userFiles
 
-    return userFiles
-
-if __name__=="__main__":main()
+if __name__=="__main__":
+    a = QReddit()
+    a.performAction()
