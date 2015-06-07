0498747ed249bebfae8afcf256aeb0358f81a3d3
icedvariables <macodeblacko@gmail.com>
2015-06-07 19:24:47 +0100

    Removed annoying debug messages

--- reddit.py
+++ reddit.py
@@ -3,11 +3,9 @@ import praw, time, sys
 class Reddit:
     def __init__(self, userAgent="QuickReddit: http://github.com/icedvariables/quickreddit /u/icedvariables"):
         self.r = praw.Reddit(user_agent=userAgent)
-        print "Created reddit object"
 
     def login(self, user, password):
         self.r.login(user, password)
-        print "Logged in"
 
     def doTextPost(self, options, user):
         self.login(user["username"], user["password"])
